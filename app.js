95f52c24a72748dfa0ec988bd3c0cd7810b81b71
Alejandro Martin <alex030293@hotmail.es>
2015-09-24 16:30:16 +0200

    fixes

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+app.get('/refresh', function(req, res, next){
+    console.log('[DEBUG]    Refresh request received');
+    require('./routes/crawler').update_posts();
+    res.end();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
